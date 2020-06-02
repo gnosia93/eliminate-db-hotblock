cf6b6c1449b5acd44e35371fbf8a6c89576e51b1
Kwon <soonbeom@f8ffc2077dc2.ant.amazon.com>
2020-06-03 02:40:02 +0900

    ..

--- scripts/after_install.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-#java -jar ./target/mbp-0.0.1-SNAPSHOT.jar --server.port=80
-nohup java -Dspring.profiles.active=prod -jar /home/ec2-user/deploy/target/mbp-0.0.1-SNAPSHOT.jar --server.port=80 > /home/ec2-user/tomcat.log 2>&1 &
-exit 0
-
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 
 export APP_HOME=/home/ec2-user/demo-cache
 echo $APP_HOME; cd $APP_HOME
-echo "when first execution, it takes about 3 minitues for required java packages download"; mvn package
+echo "At first execution, it takes about 3 minitues for required java packages download"; mvn package
 echo "starting web application"
 nohup java -Dspring.profiles.active=prod -jar $APP_HOME/target/democache-0.0.1-SNAPSHOT.jar --server.port=8080 > $APP_HOME/tomcat.log 2>&1 &
 ps aux | grep java
