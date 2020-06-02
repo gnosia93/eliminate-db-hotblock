4e881856c731a1840cc2cc8b4cc15fcd789dd64d
Kwon <soonbeom@f8ffc2077dc2.ant.amazon.com>
2020-06-03 01:56:43 +0900

    ...

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 APP_HOME=/home/ec2-user/demo-cache
-echo $APP_HOME
+echo $APP_HOME; cd $APP_HOME
+mvn package
 nohup java -Dspring.profiles.active=prod \
 -jar $APP_HOME/target/mbp-0.0.1-SNAPSHOT.jar \
 --server.port=8080 > /home/ec2-user/tomcat.log 2>&1 &
