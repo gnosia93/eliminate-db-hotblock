9a837a130eeebeedee4d898a185dd59b7d8a5d17
Kwon <soonbeom@f8ffc2077dc2.ant.amazon.com>
2020-06-03 02:00:20 +0900

    ...

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,9 +2,9 @@
 
 APP_HOME=/home/ec2-user/demo-cache
 echo $APP_HOME; cd $APP_HOME
-mvn package
+echo "at first execution, maven will download required java packages about for 3 min.."; mvn package
 nohup java -Dspring.profiles.active=prod \
--jar $APP_HOME/target/mbp-0.0.1-SNAPSHOT.jar \
---server.port=8080 > /home/ec2-user/tomcat.log 2>&1 &
+           -jar $APP_HOME/target/mbp-0.0.1-SNAPSHOT.jar \
+           --server.port=8080 > /home/ec2-user/tomcat.log 2>&1 &
 
 
