0fb14e0be389ecd95c2d53ea1fb0df7d8edd6c5d
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2020-06-03 00:02:04 +0900

    Update create-schema.sh

--- init/sql/create-schema.sh
+++ init/sql/create-schema.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mysql -u demo -p -h git-db.cluster-cjywfhnks8rs.ap-northeast-1.rds.amazonaws.com
+mysql -u demo -p -h git-db.cluster-cjywfhnks8rs.ap-northeast-1.rds.amazonaws.com < aurora.sql
