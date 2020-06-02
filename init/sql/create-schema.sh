d0c0db6b32b4cd13bd8865a7591f15155dccf4d1
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2020-06-03 00:30:52 +0900

    Update create-schema.sh

--- init/sql/create-schema.sh
+++ init/sql/create-schema.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 mysql -u demo -pdemo12345 -h git-db.cluster-cjywfhnks8rs.ap-northeast-1.rds.amazonaws.com < aurora.sql
+mysql -u demo -pdemo12345 -h git-db.cluster-cjywfhnks8rs.ap-northeast-1.rds.amazonaws.com -e "select count(1) as 'gen_product_cnt' from shop.product"
+
