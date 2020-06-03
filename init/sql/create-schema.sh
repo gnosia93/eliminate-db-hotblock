ca7b6f5b57b9fea920600fc90f53bf48ab4d0a2f
Kwon <soonbeom@f8ffc2077dc2.ant.amazon.com>
2020-06-03 22:29:43 +0900

    ..

--- init/sql/create-schema.sh
+++ init/sql/create-schema.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-mysql -u demo -pdemo12345 -h git-db.cluster-cjywfhnks8rs.ap-northeast-1.rds.amazonaws.com < aurora.sql
-mysql -u demo -pdemo12345 -h git-db.cluster-cjywfhnks8rs.ap-northeast-1.rds.amazonaws.com -e "select count(1) as 'gen_product_cnt' from shop.product"
+AURORA=<your-aurora-address>
+mysql -u demo -pdemo12345 -h $AURORA < aurora.sql
+mysql -u demo -pdemo12345 -h $AURORA -e "select count(1) as 'gen_product_cnt' from shop.product"
 
