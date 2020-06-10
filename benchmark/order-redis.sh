e35ee5c0ff0eb70582bde79d7a5d90db2fc64bf1
Kwon <soonbeom@f8ffc2077dc2.ant.amazon.com>
2020-06-11 02:35:39 +0900

    ..

--- benchmark/order-redis.sh
+++ benchmark/order-redis.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-host=git-webappa-1x1n85amayiwf-759140899.ap-northeast-1.elb.amazonaws.com:8080
+host=cachedemo-api-alb-177367678.ap-northeast-1.elb.amazonaws.com
 target=http://$host/order//event-add
 ab -l -p order-payload.json -T 'application/json;charset=utf-8' -e order-payload.csv -c 150 -n 3000 $target
