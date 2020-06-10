8c41af3f56bb47d18ace4ea05a7619dbd63f546f
Kwon <soonbeom@f8ffc2077dc2.ant.amazon.com>
2020-06-10 20:53:45 +0900

    ..

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,6 @@ export APP_HOME=/home/ec2-user/demo-cache
 echo $APP_HOME; cd $APP_HOME
 echo "At first execution, it takes about 3 minitues for required java packages download"; mvn package
 echo "starting web application"
-nohup java -Dspring.profiles.active=prod -jar $APP_HOME/target/democache-0.0.1-SNAPSHOT.jar --server.port=8080 > $APP_HOME/tomcat.log 2>&1 &
+sudo nohup java -Dspring.profiles.active=prod -jar $APP_HOME/target/democache-0.0.1-SNAPSHOT.jar --server.port=80 > $APP_HOME/tomcat.log 2>&1 &
 ps aux | grep java
 
