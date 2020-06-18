f05862eb18fcd9e751d6f4a0060a3dda9080e5f1
Kwon <soonbeom@f8ffc2077dc2.ant.amazon.com>
2020-06-18 18:02:28 +0900

    ..

--- scripts/check_order.sh
+++ scripts/check_order.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysql -u demo -pdemo12345 -h git-db.cluster-cjywfhnks8rs.ap-northeast-1.rds.amazonaws.com -e "select * from shop.order order by order_id desc"
+mysql -u demo -pdemo12345 -h <your-aurora-writer-endpoint> -e "select * from shop.order order by order_id desc"
--- scripts/check_redis.sh
+++ scripts/check_redis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-REDIS=git-cache.yv3a3x.ng.0001.apne1.cache.amazonaws.com
+REDIS=<your-redis-cluster-endpoint>
 telnet $REDIS 6379
-get sell_cnt_100
+get sell_cnt_1004
